1942ec7379e15f47525553e2e2eeb4f024ae9923
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-04-05 11:27:49 +0200

    growth_tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Poppins'],
+      },
+      colors: {
+        primary: '#304f95',
+      },
+      gridTemplateRows: {
+        two: '400px 1fr',
+      },
+    },
+  },
+  plugins: [],
+};
